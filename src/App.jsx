b720bb409baedea53bc4a9c5262310b54c57ec4b
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-22 12:57:32 +0100

    added redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Login from './features/auth/Login.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Login />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+
+export const store = configureStore({
+    reducer: {
+
+    },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware().concat(),
+    devTools: true
+})
+
+setupListeners(store.dispatch)
